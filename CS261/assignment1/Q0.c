bf448f7df6a3e0c6d8ade1d19501afafc7c817bc
patemoo <patemoo@comcast.net>
2018-01-17 08:27:24 -0800

    rename makefile, start q0

--- CS261/assignment1/Q0.c
+++ CS261/assignment1/Q0.c
@@ -1,6 +1,6 @@
 /* CS261- Assignment 1 - Q. 0*/
-/* Name:
- * Date:
+/* Name: Patrick Moore
+ * Date: January 16, 2018
  * Solution description:
  */
  
@@ -9,21 +9,28 @@
 
 void fooA(int* iptr){
      /*Print the value pointed to by iptr*/
+     printf("Value pointed to by iptr: %d \n", *iptr);
      
      /*Print the address pointed to by iptr*/
+     printf("Address pointed to by iptr: %p \n", iptr);
      
      /*Print the address of iptr itself*/
+     printf("Address of iptr itself: %p \n", iptr);
 }
 
 int main(){
     
     /*declare an integer x*/
+    int x = 5;
     
     /*print the address of x*/
+    printf("Address of x is: %p \n", &x);
     
     /*Call fooA() with the address of x*/
+    fooA(&x);
     
     /*print the value of x*/
+    printf("Value of x: %d \n", x);
     
     return 0;
 }
