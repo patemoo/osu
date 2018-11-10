e99c7c153bd5afa55fa8e389cd328f8db7f6c029
patemoo <patemoo@comcast.net>
2018-11-10 06:00:21 -0800

    update port

--- CS290/week7/index.js
+++ CS290/week7/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 8675);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
